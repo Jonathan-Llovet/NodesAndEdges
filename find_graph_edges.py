72b5f7a7922e69ce79483a2ec3d9a23b59b672c2
Jonathan Llovet <Jonathan.Llovet@gmail.com>
2019-01-04 00:59:28 -0500

    Add exception handling to find_all_edges function

--- find_graph_edges.py
+++ find_graph_edges.py
@@ -2,20 +2,28 @@ def find_all_edges(nodes):
     '''Finds the number of edges within a graph with the specified number of nodes,
     where there is a single connection between every node.
     An example application of this is to determine how many distinct pairs are in a group.'''
-    if nodes < 1:
-        message = "You must enter a positive number of nodes"
+    try:
+        if type(nodes) != int:
+            message = "You must enter a positive integer indicating the number of nodes."
+            return message
+        if nodes < 1:
+            message = "You must enter a positive integer indicating the number of nodes."
+            return message
+        elif nodes == 1:
+            total_edges = 0
+            return total_edges
+        elif nodes == 2:
+            total_edges = 1
+            return total_edges
+        else:
+            peripheral_edges = nodes
+            inner_edges = find_inner_edges(nodes)
+            total_edges = inner_edges + peripheral_edges
+            return total_edges
+    except:
+        message = "You must enter a positive integer indicating the number of nodes."
         return message
-    elif nodes == 1:
-        total_edges = 0
-        return total_edges
-    elif nodes == 2:
-        total_edges = 1
-        return total_edges
-    else:
-        peripheral_edges = nodes
-        inner_edges = find_inner_edges(nodes)
-        total_edges = inner_edges + peripheral_edges
-        return total_edges
+
 
 def find_inner_edges(nodes):
     if nodes < 3:
@@ -31,7 +39,8 @@ def find_inner_edges(nodes):
             inner_edges += m
             m -= 1
         return inner_edges
-    
+
+
 if __name__ == "__main__":
     graphs = []    
     for i in range(1,11):
