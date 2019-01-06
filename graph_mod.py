22fc2714daae7e3beea7e1da1277695074e3b3ce
Jonathan Llovet <Jonathan.Llovet@gmail.com>
2019-01-06 01:19:08 -0500

    Initial commit of graph_mod module
    Coauthors: Donald Lafleur and Jonathan Llovet.
    Calculates the number of times that a graph with a given number
    of nodes is traversed by using a given step size between nodes.
    This is done by using modular arithmetic, so that each node in
    a graph is represented by an integer in base x, where x is the
    number of nodes in the graph.
    
    To do: Clean up print formatting; extract footer to function;
    Introduce interface for arrays containing results;
    add __main__ check

---
+++ graph_mod.py
@@ -0,0 +1,48 @@
+# Base, i.e. number of nodes - this will be used as the x in n % x
+# base = int(input("Enter an integer to serve as the base:\n"))
+# # Incrementor
+# steps = int(input("Enter the number of steps that should be incremented:\n"))
+# 
+
+base = 150
+steps = 6
+def print_header(base):
+    print(f"\n\tbase = {base}\n")
+    print(f"*\tpos\t*\tsum\t*")
+    print("*"*33)
+
+def add_em_up(base, steps):
+    # nodes_hit = []
+    # values_hit = []
+    sum_of_nodes_hit = steps
+    nodes_total = 1
+
+    print(f"\n*\t0\t*\t0\t*")
+    # Prints the initial position
+    print(f"*\t{steps}\t*\t{steps}\t*")
+    # Prints the first step and the base
+    positions_hit = []
+    running_sums = []
+    while (sum_of_nodes_hit + steps) % base != steps:
+        sum_of_nodes_hit += steps
+        positions_hit.append(sum_of_nodes_hit % base)
+        running_sums.append(sum_of_nodes_hit)
+        # positions
+        nodes_total += 1    
+
+        print(f"*\t{sum_of_nodes_hit % base}\t*\t{sum_of_nodes_hit}\t*")
+
+    # sum_of_nodes_hit += steps
+    # print(f"{sum_of_nodes_hit}\t{sum_of_nodes_hit % base}")
+    print("*"*33)
+    print(f"We have traversed our graph {int(sum_of_nodes_hit / base)} times!")
+    print(f"We have landed on {nodes_total} nodes in our journey around the graph!")
+    if nodes_total == base:
+        print(f"{steps} and {base} are coprime!")
+    else:
+        print(f"Thanks for asking. Come again.")   
+
+print_header(base)
+add_em_up(base, steps)
+
+    
